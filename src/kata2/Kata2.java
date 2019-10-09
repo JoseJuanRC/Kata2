7b7c9138018d82fb7228ea2da99747171a6276e6
Usuario <Usuario@10.22.148.97>
2019-10-09 13:10:28 +0100

    Kata2 finished

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,10 @@
+package kata2;
+
+//int... values (le pasamos varios elementos como parametros y lo considea un array)
+public class Kata2 {
+
+    public static void main(String[] args) {
+         
+    }
+    
+}
