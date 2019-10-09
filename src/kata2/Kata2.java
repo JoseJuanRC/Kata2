dcdef8ac0bafd9d5e8d0418e935df62d13803067
Usuario <Usuario@10.22.148.97>
2019-10-09 13:32:14 +0100

    Primer Histograma

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -4,7 +4,8 @@ package kata2;
 public class Kata2 {
 
     public static void main(String[] args) {
-         
+        Histogram histogram = new Histogram(1,2,3,4,-4,4,4,4,5,1,4,0,0,1,0,52,-4);
+        new ConsoleHistogramDisplaya().show(histogram);
     }
     
 }
